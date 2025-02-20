9b753656779cba442125ec8822e3ef2f51cdca0e
Sunny Yu <sunny@Sunnys-MacBook-Pro.local>
2025-02-19 20:00:14 -0800

    add fish dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -g fish_greeting
+    starship init fish | source
+
+	function y
+		set tmp (mktemp -t "yazi-cwd.XXXXXX")
+		yazi $argv --cwd-file="$tmp"
+		if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+			builtin cd -- "$cwd"
+		end
+		rm -f -- "$tmp"
+	end
+end

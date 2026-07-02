0e17baa264b35bbdbbd47646519cbef7e31ad196
Sunny Yu <sunny@Sunnys-MacBook-Pro.local>
2026-07-01 23:21:29 -0700

    Update Tmux and Neovim Options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.clipboard = ""
+vim.g.autoformat = false
+
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+  pattern = "*",
+  command = "checktime",
+})
+vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave", "InsertLeave" }, {
+  pattern = "*",
+  command = "silent! update",
+})
